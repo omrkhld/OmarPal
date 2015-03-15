092f954a0eac52481984165074a4e4a5a3564ea2
Omar Khalid <omar.khalid.yahya@gmail.com>
2015-03-15 21:29:24 +0800

    Initial Commit. Files added: Paypal Express Checkout code E-Commerce template from http://www.bootstrapzero.com/bootstrap-template/e-commerce Shopping Cart example from http://www.sanwebe.com/2013/06/creating-simple-shopping-cart-with-php Bootstrap zip

---
+++ Checkout/cancel.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import header
+print """\
+<span class="span4">
+	</span>
+	<span class="span5">
+		<div class="hero-unit">
+		   You cancelled the order.
+		</div>
+	</span>
+	<span class="span3">
+	</span>"""
+import footer
