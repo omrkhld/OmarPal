8b3f30d244b8b9a8b1f8613fa8eecd81fcfed3f8
Omar Khalid <omar.khalid.yahya@gmail.com>
2015-03-17 01:30:41 +0800

    Added assets. Minor updates to Checkout files. Need to fix E_NOTICE echo. Need to fix Carousel in Modal in index.html. Need to make cart.html customizable.

--- paypal-shopping-cart-example/config.php
+++ paypal-shopping-cart-example/config.php
@@ -10,9 +10,9 @@ $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 
 //paypal settings
 $PayPalMode 			= 'sandbox'; // sandbox or live
-$PayPalApiUsername 		= 'paypaluser@somemail.com'; //PayPal API Username
-$PayPalApiPassword 		= '190798792445'; //Paypal API password
-$PayPalApiSignature 	= '797987709709709oipuiou98Eq.Gufar'; //Paypal API Signature
+$PayPalApiUsername 		= 'omar.khalid.yahya-facilitator_api1.gmail.com'; //PayPal API Username
+$PayPalApiPassword 		= '4SLWKZYU86VQDTJC'; //Paypal API password
+$PayPalApiSignature 	= 'AFcWxV21C7fd0v3bYYYRCpSSRl31AwJqMrChK.ycHT8TMQE2DH1PTppp'; //Paypal API Signature
 $PayPalCurrencyCode 	= 'USD'; //Paypal Currency Code
 $PayPalReturnURL 		= 'http://localhost/shopping-cart/paypal-express-checkout/process.php'; //Point to process.php page
 $PayPalCancelURL 		= 'http://localhost/shopping-cart/paypal-express-checkout/cancel_url.html'; //Cancel URL if user clicks cancel
